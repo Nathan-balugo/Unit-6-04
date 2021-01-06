5be4ae2966564ce0364951f5b45c459e89526144
Nathanbalugo <Nathan.balugo@ocsbstudent.ca>
2021-01-06 14:20:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let age = prompt("Enter your age")
+let day = "Monday"
+if ((day == "Thursday" || day == "Tuesday") || (age
+>=12 && age <=21)) {
+  alert ("You may enter the museum")
+}
+else {
+  alert ("you may not enter the museum")
+}
